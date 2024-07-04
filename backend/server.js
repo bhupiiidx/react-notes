1cc8efe02d61a259f505a44d5927bf5eb5dc4daf
bhupiiidx <bhupender81238@gmail.com>
2024-07-04 12:48:56 +0530

    frontend-initialization

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.BACKEND_PORT || 5000;
+const notes = require("./data/todo_data.json");
+
+app.get("/", function (req, res, next) {
+	res.send("Welcome");
+});
+
+app.get("/api/notes", function (req, res, next) {
+	res.send(notes);
+});
+
+app.get("/api/note/:note_id", function (req, res, next) {
+	const { note_id } = req.params;
+	res.send(notes.find((note) => note.id == note_id));
+});
+
+app.listen(PORT, console.log("Server listening on PORT " + PORT));
